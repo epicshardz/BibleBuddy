48bd8bbac34f6a510be6d3c016763c336ccbfab6
epicshardz <mrsmirf7@gmail.com>
2024-06-03 15:50:39 -0700

    Major Updates: Minor Prompt Update

--- conversationalQAchain.js
+++ conversationalQAchain.js
@@ -1,7 +1,7 @@
 const { ChatOpenAI, OpenAIEmbeddings } = require("@langchain/openai");
 const { ConversationalRetrievalQAChain } = require("langchain/chains");
 const { QdrantVectorStore } = require("@langchain/qdrant");
-const { ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, PromptTemplate } = require("@langchain/core/prompts");
+const { PromptTemplate } = require("@langchain/core/prompts");
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,7 +10,7 @@ dotenv.config();
 const maxTokens = 3000;
 
 /* Clean text function */
-function cleanText(text) {
+function cleanText(text, maxTokens) {
     if (text) {
         text = text.replace(/\s+/g, ' ');  // Replace multiple whitespace with a single space
         text = text.replace(/[^\x20-\x7E]/g, '');  // Remove non-ASCII characters
@@ -36,27 +36,7 @@ class BibleBuddyQA {
             openAIApiKey: process.env.OPENAI_API_KEY,
         });
 
-        this.systemTemplate = `
-        Use the following pieces of context to try to answer the users question. 
-        If you don't know the answer, politely ask for clarification or a followup question! I repeat, only answer based on the context or to followup on a previous query. If the answer is in the {bible} but not in the context provided please answer using that known scripture.
-        ALWAYS return a scripture "SOURCES" part in your answer. The scripture "sources" part should be a reference to the verse/verses of the document from which you got your answer.
-        
-        Example of your response should be:
-        
-        The answer is foo
-
-        sources: xyz
-        
-        Begin!
-        ----------------
-        {context}`;
-
-        this.messages = [
-            SystemMessagePromptTemplate.fromTemplate(this.systemTemplate),
-            HumanMessagePromptTemplate.fromTemplate("{question}")
-        ];
-        this.prompt = ChatPromptTemplate.fromMessages(this.messages);
-        this.qa_template = `Use the following pieces of context to try to answer the users question. 
+        this.qa_template = `Use the following pieces of context to try to answer the users question. Provide a complete response with sufficient explanation and bulleted points. 
         If you don't know the answer, politely ask for clarification or a followup question! I repeat, only answer based on the context or to followup on a previous query. If the answer is in the christian bible but not in the context provided please answer using that known scripture.
         ALWAYS return a scripture "SOURCES" part in your answer. The scripture "sources" part should be a reference to the verse/verses of the document from which you got your answer.
         
@@ -100,7 +80,10 @@ class BibleBuddyQA {
             }),
             retriever,
             {
-                qaPrompt: PromptTemplate.fromTemplate(this.qa_template),
+                qaChainOptions: {
+                    type: "stuff",
+                    prompt: PromptTemplate.fromTemplate(this.qa_template),
+                },
                 returnSourceDocuments: true,
                 // verbose: true,
 
@@ -118,9 +101,9 @@ class BibleBuddyQA {
         }
 
         const chain = await this.createChain(bible);
-        const question = cleanText(inputData.prompt);
-        const lastResponse = cleanText(inputData.last_response);
-        const lastPrompt = cleanText(inputData.last_prompt);
+        const question = cleanText(inputData.prompt, 1000);
+        const lastResponse = cleanText(inputData.last_response, 1000);
+        const lastPrompt = cleanText(inputData.last_prompt, 1000);
 
         const chatHistory = `${lastPrompt}\n${lastResponse}`;
 
